a6f821c36966eabd4d26f907ece72324d2e98aa5
david-allison12 <dgreenal@gmail.com>
2025-10-15 18:25:16 -0400

    Add borrower creation route plus schema

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from marshmallow import ValidationError
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+
+from models import db, Borrower, Application
+from schemas import BorrowerRequestSchema, BorrowerSchema, ApplicationRequestSchema, ApplicationSchema
 
 
 bp = Blueprint("api", __name__)
@@ -16,9 +21,40 @@ def health():
 # Borrowers Endpoints
 # ========================================
 
+bp.route("/borrowers", methods=["POST"])
+def create_borrower():
+    req_schema = BorrowerRequestSchema()
+    res_schema = BorrowerSchema()
 
+    try:
+        data = req_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    # Check for existing SSN
+    existing_borrower = Borrower.query.filter_by(ssn=data['ssn']).first()
+    if existing_borrower:
+        return jsonify({"error": "Borrower with this SSN already exists."}), 409 # Conflict Status Code
+    
+    # Create new borrower
+    try:
+        new_borrower = Borrower(**data)
+        db.session.add(new_borrower)
+        db.session.commit()
 
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"error": "Database integrity error: Please check for duplicates or missing requered fields."}), 400
 
+    except SQLAlchemyError as e:
+            db.session.rollback()
+            return jsonify({"error": "An unexpected database error occurred.", "details": str(e)}), 500
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":"Unexpecred server error", "details": str(e)}), 500
+    
+    return res_schema.jsonify(new_borrower), 201
 
 # ========================================
 # Applications Endpoints
---
+++ backend/schemas.py
@@ -0,0 +1,28 @@
+from app import ma
+from models import Borrower, Application
+from marshmallow import fields
+
+
+# ========================================
+# Borrowers Schemas
+# ========================================
+
+# Validate Incoming POST data 
+class BorrowerRequestSchema(ma.Schema):
+    name = fields.String(required=True)
+    address_1 = fields.String(required=True)
+    address_2 = fields.String()
+    city = fields.String(required=True)
+    state = fields.String(required=True)
+    zip_code = fields.String(required=True)
+    email = fields.Email(required=True)
+    phone = fields.String()
+    ssn = fields.String(required=True) # Mask/encrypt at later date for production app
+
+
+# Serialize Outgoing Response data
+class BorrowerSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Borrower
+        load_instance = True
+        exclude = ("ssn",)  # Exclude SSN from response for security
