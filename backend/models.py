eef3b1c6fce4b1d008a0eee76c03346d725f8323
david-allison12 <dgreenal@gmail.com>
2025-10-14 20:51:41 -0400

    added models

---
+++ backend/models.py
@@ -0,0 +1,26 @@
+from app import db
+
+class Borrower(db.Model):
+    borrower_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False) # Street address, required for most users
+    email = db.Column(db.String(100), nullable=False)
+    address_1 = db.Column(db.String(200), nullable=False) # Street address, required for most users
+    address_2 = db.Column(db.String(200))
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    zip_code = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20))
+    ssn = db.Column(db.String(11), unique=True, nullable=False)  # Format: XXX-XX-XXXX
+
+
+class Application(db.Model):
+    application_id = db.Column(db.Integer, primary_key=True)
+    borrower_id = db.Column(db.Integer, db.ForeignKey('borrower.borrower_id'), nullable=False)
+    requested_amount = db.Column(db.Float, nullable=False)
+    open_credit_lines = db.Column(db.Integer, nullable=False)
+    approved_amount = db.Column(db.Float)
+    interst_rate = db.Column(db.Float)
+    term_months = db.Column(db.Integer)
+    monthly_payment = db.Column(db.Float)
+    status = db.Column(db.String(50), nullable=False, default='Pending')  
+    reason = db.Column(db.String(200))
--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,25 @@
 from flask import Blueprint, jsonify
 
 
-bp = Blueprint("health", __name__)
+bp = Blueprint("api", __name__)
 
+
+# ========================================
+# Health Check Endpoint
+# ========================================
 @bp.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "ok"}), 200
+
+
+# ========================================
+# Borrowers Endpoints
+# ========================================
+
+
+
+
+
+# ========================================
+# Applications Endpoints
+# ========================================
